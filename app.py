13749e0a5447a709309cdd56225c1c0a000d8dd2
古田英彦 <furutahidehiko@furutahidehikonoMacBook-Pro.local>
2023-09-28 12:46:37 +0900

    ユーザー登録画面修正

--- app.py
+++ app.py
@@ -9,24 +9,27 @@ from json_serial import json_serial
 page = st.sidebar.selectbox('Choose your page',['users','rooms','bookings'])
 
 if page == 'users':
-  st.title('APIテスト画面(ユーザー)')
+  st.title('ユーザー登録画面')
 
   with st.form(key='user'):
-    user_id: int = random.randint(0,10) # 0~10のランダムな整数を生成
+    # user_id: int = random.randint(0,10) # 0~10のランダムな整数を生成
     username: str = st.text_input('ユーザー名',max_chars=12) # 12字文字以内
     data = {
-      'user_id':user_id,
+      # 'user_id':user_id,
       'username':username
     }
     submit_button = st.form_submit_button(label='送信') # form専用ボタン
 
   if submit_button: # ボタンが押されたかどうか
     st.write('## 送信データ') # デバック用
-    st.json(data) # どんなデータが入っているか確認するため
+    # st.json(data) # どんなデータが入っているか確認するため
     st.write('## レスポンス結果')
     url = 'http://127.0.0.1:8000/users'
     res = requests.post(url,data=json.dumps(data))
-    st.write(res.status_code)
+    if res.status_code == 200:
+      st.success('ユーザー登録完了')
+    else:
+      st.error('ユーザー登録失敗')
     st.json(res.json())
 
 elif page == 'rooms':
