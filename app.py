b55a6fe72f4475c14ab482230230841eeef1c58c
古田英彦 <furutahidehiko@furutahidehikonoMacBook-Pro.local>
2023-09-28 13:22:35 +0900

    会議室予約画面作成

--- app.py
+++ app.py
@@ -12,16 +12,12 @@ if page == 'users':
   st.title('ユーザー登録画面')
 
   with st.form(key='user'):
-    # user_id: int = random.randint(0,10) # 0~10のランダムな整数を生成
     username: str = st.text_input('ユーザー名',max_chars=12) # 12字文字以内
-    data = {
-      # 'user_id':user_id,
-      'username':username
-    }
+    data = {'username':username}
     submit_button = st.form_submit_button(label='送信') # form専用ボタン
 
   if submit_button: # ボタンが押されたかどうか
-    st.write('## 送信データ') # デバック用
+    # st.write('## 送信データ') # デバック用
     # st.json(data) # どんなデータが入っているか確認するため
     st.write('## レスポンス結果')
     url = 'http://127.0.0.1:8000/users'
@@ -33,26 +29,29 @@ if page == 'users':
     st.json(res.json())
 
 elif page == 'rooms':
-  st.title('APIテスト画面(会議室)')
+  st.title('会議室登録画面')
 
   with st.form(key='room'):
     room_id: int = random.randint(0,10) # 0~10のランダムな整数を生成
     room_name: str = st.text_input('会議室名',max_chars=12) # 12字文字以内
     capacity: int = st.number_input('定員',step=1)
     data = {
-      'room_id':room_id,
       'room_name':room_name,
       'capacity':capacity
     }
     submit_button = st.form_submit_button(label='送信') # form専用ボタン
 
   if submit_button: # ボタンが押されたかどうか
-    st.write('## 送信データ') # デバック用
-    st.json(data) # どんなデータが入っているか確認するため
+    # st.write('## 送信データ') # デバック用
+    # st.json(data) # どんなデータが入っているか確認するため
     st.write('## レスポンス結果')
     url = 'http://127.0.0.1:8000/rooms'
     res = requests.post(url,data=json.dumps(data))
-    st.write(res.status_code)
+    if res.status_code == 200:
+      st.success('会議室登録完了')
+    else:
+      st.error('会議室登録失敗')
+    # st.write(res.status_code)
     st.json(res.json())
 
 elif page == 'bookings':
